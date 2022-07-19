f0ef96c1232597f05346cee5c98f5fe786a8fb83
Benjamin Reynes <benjamin@morio.co>
2022-07-19 08:50:34 +0200

    trad

--- client/src/components/NotConnectedInfo/NotConnectedInfo.tsx
+++ client/src/components/NotConnectedInfo/NotConnectedInfo.tsx
@@ -2,7 +2,7 @@ const NotConnectedInfo = () => {
   return (
     <div className="h-56 overflow-hidden  rounded-md border bg-cover text-gray-100">
       <div className="relative flex h-full w-full flex-col items-center justify-center bg-slate-800 p-10">
-        Vous devez vous connecter
+        Please connect your wallet
       </div>
     </div>
   )
